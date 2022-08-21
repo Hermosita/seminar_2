8ecba78c2d770969c822c614b891350720f325c9
Hermosita <Helen-aquarius@yandex.ru>
2022-08-21 21:18:33 +0300

    add problem29

---
+++ задача_29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+int[] numbers = new int[15];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+
+array[i] = new Random().Next(0, 20); }
+
+void PrintArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+}
+
+
+
+
+
+ 
+ 
+            
+
